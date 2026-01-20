918fe4afc5b84bcd2fd87d2466adc893d317fe50
filipebezerra95 <filipebza2019@gmail.com>
2026-01-19 22:33:02 -0300

    configurando o vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/ngb-marketing-site/",
-  plugins: [react()],
-})
+  build: {
+    outDir: "."
+  },
+  plugins: [react()]
+});
+
