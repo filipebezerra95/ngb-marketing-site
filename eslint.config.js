d46ede23871b616dd2bbc8877e233daba39694b0
filipebezerra95 <filipebza2019@gmail.com>
2026-01-19 23:48:17 -0300

    resolvendo problemas no deploy

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,14 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{js,jsx}'],
+    ignores: ['dist', 'node_modules/**', 'dist/**'],
+  },
+  {
+    files: ['src/**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
